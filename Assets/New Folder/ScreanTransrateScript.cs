cbcbaf9935cc50d392a3f6c6c37dba492221a0a5
戸澤海智 <st37071556@icloud.com>
2021-08-02 15:14:10 +0900

    amidaに利用可能なスクリプト コンソール座標とワールド座標を変換 Z軸を合わせるのを忘れないように

---
+++ Assets/New Folder/ScreanTransrateScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreanTransrateScript : MonoBehaviour
+{
+    [SerializeField] Transform m_someObject = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector2 screenPosition = Input.mousePosition;
+            var worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+            Debug.Log(worldPosition.ToString());
+            worldPosition.z = 0;
+            m_someObject.position = worldPosition;
+        }
+    }
+}
